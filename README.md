6aacc0f0faf960b518e4982294224e90a8abf7d3
Roma104 <92829743+Roma104@users.noreply.github.com>
2022-04-29 11:22:02 +0200

    2022-04-29 RB

--- README.md
+++ README.md
@@ -4,15 +4,15 @@
 
 |Składowa|Selektor|Nazwa zmiennej|Typ zmiennej|
 |--------|--------|--------------|------------|
-|opinia|div.js_product-review|||
-|identyfikator opinii|div.js_product-review\["data-entry-id"\]|||
-|autor opinii|span.user-post__author-name|||
-|rekomendacje|span.user-post__author-recomendation  > em|||
-|liczba gwiazdek|span.user-post__score-count|||
-|treść opinii||||
-|lista zalet|div.review-feature__title--positives ~ div.review-feature__item |||
-|lista wad|div.review-feature__title--negatives ~ div.review-feature__item|||
-|dla ilu osób przydatna|span[id^="votes-yes"]|||
-|dla ilu osób nieprzydatna|span[id^="votes-no"]|||
-|dla wystawienia mopinii|span.user-post__published > time:nth-child(1)["datetime"]|||
-|data zakupu|span.user-post__published > time:nth-child(2)["datetime"]|||
+|opinia|div.js_product-review|opinion|bs4.element.Tag|string|
+|identyfikator opinii|div.js_product-review\["data-entry-id"\]|opinion_id|14133566|string|
+|autor opinii|span.user-post__author-name|author|Maria|string|
+|rekomendacje|span.user-post__author-recomendation  > em|recomendation|Polecam|string|
+|liczba gwiazdek|span.user-post__score-count|stars|5/5|string|
+|treść opinii|div.user-post__text|content|Lodówka amica jest warta zakupu|string|
+|lista zalet|div.review-feature__title--positives ~ div.review-feature__item |pros|list|
+|lista wad|div.review-feature__title--negatives ~ div.review-feature__item|conts|list|
+|dla ilu osób przydatna|span[id^="votes-yes"]|useful|string|
+|dla ilu osób nieprzydatna|span[id^="votes-no"]|useless|string|
+|dla wystawienia mopinii|span.user-post__published > time:nth-child(1)["datetime"]|publish_date|string|
+|data zakupu|span.user-post__published > time:nth-child(2)["datetime"]|purchase_date|string|
--- scraper.py
+++ scraper.py
@@ -1,5 +1,58 @@
+#from cgitb import html
+from inspect import Attribute
 import requests
+from bs4 import BeautifulSoup
+import json
 
-url = "https://www.ceneo.pl/91714422#tab=reviews"
-response = requests.get(url)
-print(response.status_code)
+url = "https://www.ceneo.pl/91714422#4tab=reviews"
+
+all_opinions = []
+while(url):
+    response = requests.get(url)
+    page = BeautifulSoup(response.text,'html.parser')
+
+    opinions = page.select("div.js_product-review")
+
+    for opinion in opinions:
+        opinion_id = opinion["data-entry-id"]
+        author = opinion.select_one("span.user-post__author-name").get_text().strip()
+        try:
+            recomendation = opinion.select_one("span.user-post__author-recomendation > em").get_text().strip()
+        except AttributeError:
+            recomendation = None
+        stars = opinion.select_one("span.user-post__score-count").get_text().strip()
+        content = opinion.select_one("div.user-post__text").get_text().strip()
+        pros = opinion.select("div.review-feature__title--positives ~ div.review-feature__item")
+        pros = [item.get_text().strip() for item in pros]
+        conts = opinion.select("div.review-feature__title--negatives ~ div.review-feature__item")
+        conts =[item.get_text().strip() for item in conts]
+        useful = opinion.select_one("button.vote-yes > span").get_text().strip()
+        useless = opinion.select_one("button.vote-no > span").get_text().strip()
+        publish_date = opinion.select_one("span.user-post__published > time:nth-child(1)").get_text().strip()
+        try:
+            purchase_date = opinion.select_one("span.user-post__published > time:nth-child(2)").get_text().strip()
+        except AttributeError:
+            purchase_date = None
+
+        single_opinion ={
+            "opinion_id":opinion_id,
+            "author": author,
+            "recomendation": recomendation,
+            "stars": stars,
+            "content": content,
+            "useful": useful,
+            "useless": useless,
+            "publish_date": publish_date,
+            "purchase_date": purchase_date,
+            "pros": pros,
+            "conts": conts
+        }
+
+        all_opinions.append(single_opinion) 
+    try:
+        url = "https://www.ceneo.pl" + page.select_one("a.pagination__next")["href"]
+    except TypeError:
+        url= None
+with open("opinions/91714422.json", "w", encoding="UTF-8") as jf:
+    json.dump(all_opinions, jf, indent= 4, ensure_ascii=False)
+
